0e5d5e61997c4b38581681569f936c8a33cfbdb3
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-07-22 15:40:34 -0400

    Basic Programming

---
+++ ITS320_CTA1_Option1.py
@@ -0,0 +1,8 @@
+# Draw a mouse with print statements
+# Author: Andrew Barnes
+
+print("          (\--.")
+print(" _)       /  _`>")
+print("(        /  _)=")
+print(" `.     /  _/")
+print("   `.__(____)__")
---
+++ ITS320_CTA5_Option1.py
@@ -0,0 +1,38 @@
+#--------------------------------------------------------------------------------
+# Program Name: Week 5 Critical Thinking Assignment
+# Program Author: Andrew Barnes
+# Program Objective: In a function, return to the user a concatenation of three 
+#                    string values in reverse order.
+#--------------------------------------------------------------------------------
+# Pseudocode: 1. Create a function that concatonates and reverses three
+#                string parameters then returns the result.
+#             2. Create an empty list for the three strings to reside.
+#             3. Allow the user to input the three strings in a for loop
+#                that appends the string to the list.
+#             4. Populate the parameters of the function with the strings from
+#                the list.
+#             5. Print the reversed and concatonated result.
+#--------------------------------------------------------------------------------
+# Program Inputs: Three Strings
+# Program Outputs: A concatonations of the three strings in reverse
+print("Reverse & Concatonate Three Strings")
+
+def concatonate_and_reverse_strings(string_1, string_2, string_3):
+    concatonate_strings = string_1 + string_2 + string_3
+    reversed_string = concatonate_strings[::-1]
+    return reversed_string
+
+strings = []
+amount_of_strings = 3
+
+for i in range(amount_of_strings):
+    string = input("Enter a string of text: ")
+    if string == "":
+        print("ERROR: A string must be supplied")
+    else:
+        strings.append(string)
+
+result = concatonate_and_reverse_strings(strings[0], strings[1], strings[2])
+print(f"Result: {result}")
+
+print("End Program")
