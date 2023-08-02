262ef592a3c22604ec9e92d928587b6278243907
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-08-01 22:56:58 -0400

    Basic Programming

---
+++ portfolio_project.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------------------------------
+# Program Name: Final Portfolio Project - Automobile Inventory Program
+# Program Author: Andrew Barnes
+# Program Objective: Create an automobile class to be used as a vehicle inventory 
+#                    program for a dealership. The program should allow the 
+#                    addition, removal and updating of vehicles in the inventory, 
+#                    and output the final inventory to a text file.
+#--------------------------------------------------------------------------------
+# Pseudocode: 1. Define the Automobile class with private attributes: make, model,
+#                color, year, and mileage.
+#             2. Define the appropriate methods in the class: constructor,
+#                add a new vehicle, remove a vehicle, and update vehicle attributes.
+#             3. Implement user interface for interacting with the inventory: adding, 
+#                removing, updating vehicles, and exporting the inventory.
+#             4. Allow the user to export the inventory to a text file at the end of 
+#                their session.
+#--------------------------------------------------------------------------------
+
+class Automobile:
+    def __init__(self, make, model, color, year, mileage):      # Constructor
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+        self.mileage = mileage
+
+    def add():                                                  # Add new vehicle
+        pass
+
+    def remove():                                               # Remove vehicle
+        pass
+
+    def update():                                               # Update vehicle
+        pass
