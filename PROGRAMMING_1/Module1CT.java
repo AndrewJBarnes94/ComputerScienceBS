8d9908dd91710a33a0db998ab43a1472d1b0758d
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-08-14 23:06:59 -0400

    Programming 1

---
+++ PROGRAMMING_1/Module1CT.java
@@ -0,0 +1,19 @@
+public class Module1CT {
+    public static void main(String[] args) {
+        String first_name, last_name, street_address, city, zipcode;
+
+        first_name = "Andrew";
+        last_name = "Barnes";
+        street_address = "1234 Main Street";
+        city = "St. Petersburg";
+        zipcode = "12345";
+
+        System.out.println("");
+        System.out.println("First Name: " + first_name);
+        System.out.println("Last Name: " + last_name);
+        System.out.println("Street Address: " + street_address);
+        System.out.println("City: " + city);
+        System.out.println("Zipcode: " + zipcode);
+        System.out.println("");
+    }
+}
