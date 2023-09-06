5eb697c6ce20750c0de8ab5b9608494cecb08fba
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-09-05 22:36:08 -0400

    Module 3 CTA

---
+++ PROGRAMMING_1/TaxWithholdingCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TaxWithholdingCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter your weekly income: $");
+        double income = scanner.nextDouble();
+        
+        double taxRate;
+        if (income < 500) {
+            taxRate = 0.10; // 10%
+        } else if (income < 1500) {
+            taxRate = 0.15; // 15%
+        } else if (income < 2500) {
+            taxRate = 0.20; // 20%
+        } else {
+            taxRate = 0.30; // 30%
+        }
+        
+        double taxWithheld = income * taxRate;
+        
+        System.out.println("Your weekly average tax withholding is: $" + taxWithheld + "\n");
+    }
+}
