e4ec280715affe9bad4c775619a4c4ecc8685ea8
Andrew Barnes <andrewjbarnes94@gmail.com>
2023-09-10 23:10:10 -0400

    Computer Science Fundamentals

--- COMPUTER_SCIENCE_FUNDAMENTALS/Module3CTA.py
+++ COMPUTER_SCIENCE_FUNDAMENTALS/Module3CTA.py
@@ -1,3 +1,24 @@
+'''
+FUNCTION bubble_sort(arr: ARRAY OF INTEGER) -> ARRAY OF INTEGER:
+    n = LENGTH OF arr
+    FOR i FROM 0 TO n-1:
+        FOR j FROM 0 TO n-i-1:
+            IF arr[j] > arr[j+1]:
+                SWAP arr[j] AND arr[j+1]
+    RETURN arr
+
+MAIN:
+    employee_salaries = [500, 300, 700, 400, 600, 550]
+
+    ascending_order = bubble_sort(COPY OF employee_salaries)
+    descending_order = REVERSE OF ascending_order
+
+    PRINT "Employee Salaries: ", employee_salaries
+    PRINT "Salaries in ascending Order: ", ascending_order
+    PRINT "Salaries in descending Order: ", descending_order
+'''
+
+
 def bubble_sort(arr):
     n = len(arr)
     for i in range(n-1):
