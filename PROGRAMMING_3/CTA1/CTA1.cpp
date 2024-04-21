acff9a67d8cabe86600f2fbd452822c9ef4bda0d
Andrew Barnes <andrewjbarnes94@gmail.com>
2024-04-21 17:16:35 -0400

    Programming3 CTA1

---
+++ PROGRAMMING_3/CTA1/CTA1.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : CTA1.cpp
+// Author      : Andrew Barnes
+// Version     : 1
+// Description : Basic Console Application for Programming 3 CTA1
+//============================================================================
+
+#include<iostream>;
+
+using namespace std;
+
+int main() {
+	string firstName = "Andrew";
+	string lastName = "Barnes";
+	string streetAddress = "123 Main Street";
+	string city = "Saint Petersburg, FL";
+	int zipCode = 12345;
+
+	cout << firstName << endl;
+	cout << lastName << endl;
+	cout << streetAddress << endl;
+	cout << city << endl;
+	cout << zipCode << endl;
+
+	return 0;
+}
